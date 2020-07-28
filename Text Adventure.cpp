72419d2e817577df5ef0268221fca6a58ca9d3f2
cdelangelm <cdelangelm@users.noreply.github.com>
2020-07-27 20:56:55 -0500

    Add project files.

---
+++ Text Adventure.cpp
@@ -0,0 +1,15 @@
+// Text Adventure.cpp : Build a text adventure.
+/*In order to complete this project, you should be familiar with C++ conditionals and control flow, loops,
+  and setting variables using user input. Ideally, you’ve finished the first four sections of Learn C++ (through Loops).
+  
+  Your program should have a storyline with:
+  
+  1. A beginning
+  2. At least three possible endings*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Your story starts with";
+}
